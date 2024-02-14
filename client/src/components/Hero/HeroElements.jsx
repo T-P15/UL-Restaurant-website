428b4b20ea198433a288177c18beeb607454f581
mingosis <tom.piccirillo15@gmail.com>
2024-02-14 19:30:40 +1000

    hero complete

--- client/src/components/Hero/HeroElements.jsx
+++ client/src/components/Hero/HeroElements.jsx
@@ -1,18 +1,29 @@
 import styled from "styled-components";
+import {MdKeyboardArrowRight, MdArrowForward} from 'react-icons/md'
+import { Link as LinkRouter} from 'react-router-dom'
 
 export const HeroContainer = styled.div`
-background: rgb(255,255,255);
-background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(247,214,206,1) 47%, rgba(255,0,48,1) 100%);
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 0 30px;
     height: 800px;
     position: relative;
-    z-index: 1
+    z-index: 1;
+
 `;
 
 export const HeroBg = styled.div`
+position: absolute;
+top: 0;
+right: 0;
+bottom: 0;
+left: 0;
+width: 100%;
+height: 100%;
+overflow: hidden;
+`
+export const HeroText = styled.div`
 z-index: 3;
 max-width: 1200px;
 position: absolute;
@@ -20,10 +31,13 @@ padding: 8px 24px;
 display: flex;
 flex-direction: column;
 align-items: center;
+background-color: #000;
+border: 1px solid black;
+opacity: 0.8;
 `;
 
 export const Heroh1 = styled.h1`
- color: #de0a26;
+ color: #fff;
  font-size: 48px;
  text-align: center;
 
@@ -34,10 +48,16 @@ export const Heroh1 = styled.h1`
     font-size: 32px;
  }
 `;
-
+export const VideoBg = styled.video`
+ width: 100%;
+ height: 100%;
+ -o-object-fit: cover;
+ object-fit: cover;
+ 
+`;
 export const HeroP = styled.p`
 margin-top: 24px;
-color: #de0a26;
+color: #fff;
 font-size: 24px;
 text-align: center;
 max-width: 600px;
@@ -48,4 +68,41 @@ max-width: 600px;
  @media screen and (max-width: 480px) {
     font-size: 18px;
  }
+`;
+
+export const HeroOrderBtn = styled.div`
+ margin-top: 32px;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
 `
+
+export const ArrowForward = styled(MdArrowForward)`
+ margin-left: 8px;
+ font-size: 20px;
+`;
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+margin-left: 8px;
+ font-size: 20px;
+`;
+
+export const OrderButton = styled(LinkRouter)`
+    border-radius: 50px;
+    background: #c30010;
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #fff;
+    font-size: 20px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration:none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #000;
+        color: #fff;
+    }
+`;
--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -1,15 +1,26 @@
-import React from "react";
-import { HeroContainer, HeroBg, HeroP, Heroh1 } from "./HeroElements";
-
+import React, {useState} from "react";
+import { HeroContainer, HeroBg, HeroP, Heroh1, VideoBg, HeroText, HeroOrderBtn, OrderButton, ArrowForward, ArrowRight} from "./HeroElements";
+import Video from '../../assets/Video.mp4'
 
 
 const Hero = () => {
+
+    const [hover, setHover] = useState(false)
+    const onHover =() => {
+        setHover(!hover)
+    }
     return (
         <HeroContainer>
             <HeroBg>
-                <Heroh1> Uncle Lais </Heroh1>
-                <HeroP> Indulge in mouth watering authentic malaysian cuisine</HeroP>
+                <VideoBg autoPlay loop muted src={Video} type = 'video/mp4' /> 
             </HeroBg>
+            <HeroText>
+                <Heroh1>Let's Makan!</Heroh1>
+                <HeroP> Come and try Uncle Lais authentic Malaysian cuisine transporting yourself to the streets of Malaysia as if you were really there!</HeroP>
+                <HeroOrderBtn>
+                    <OrderButton to="order" onMouseEnter={onHover} onMouseLeave={onHover}>Order Now {hover ? <ArrowForward /> : <ArrowRight/>} </OrderButton>
+                </HeroOrderBtn>
+            </HeroText>
         </HeroContainer>
     )
 };
