7975e3cbcc3b44abe44216e9734d2c0883795721
mingosis <tom.piccirillo15@gmail.com>
2024-02-14 21:04:31 +1000

    front end mutations made

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,53 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
 
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $mobile: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      mobile: $mobile
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_ORDER = gql`
+  mutation addOrder($menuitems: [ID]!) {
+    addOrder(menuitems: $menuitems) {
+      purchaseDate
+      menuitems {
+        _id
+        name
+        description
+        price
+        category {
+          name
+        }
+      }
+    }
+  }
+`;
 
