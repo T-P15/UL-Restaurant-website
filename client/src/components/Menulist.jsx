ea80d77c4e4c9efa3dacc61474857d67b8f1bc73
mingosis <tom.piccirillo15@gmail.com>
2024-02-16 11:02:36 +1000

    order button

--- client/src/components/Menulist.jsx
+++ client/src/components/Menulist.jsx
@@ -7,7 +7,10 @@ const MenuList = ({ menuItems }) => {
       <div class="text-center p-5 " key={menuitem._id}>
         <h3 class="text-2xl text-red-800">{menuitem.name}</h3>
         <p class="p-2 text-xl">price: {menuitem.price}</p>
-        <p class="text-xl italic">{menuitem.description}</p>
+        <p class="text-xl italic p-2">{menuitem.description}</p>
+        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded ">
+  Order Now 
+</button>
       </div>
     ))}
   </div>
