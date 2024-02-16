61028473d4945fa07419e8fa7a206b16fab1cba6
mingosis <tom.piccirillo15@gmail.com>
2024-02-16 10:52:06 +1000

    styling of menuitems

--- client/src/components/Menulist.jsx
+++ client/src/components/Menulist.jsx
@@ -4,10 +4,10 @@ const MenuList = ({ menuItems }) => {
     return (
   <div>
     {menuItems.map(menuitem => (
-      <div key={menuitem._id}>
-        <h3>{menuitem.name}</h3>
-        <p>price: {menuitem.price}</p>
-        <p>{menuitem.description}</p>
+      <div class="text-center p-5 " key={menuitem._id}>
+        <h3 class="text-2xl text-red-800">{menuitem.name}</h3>
+        <p class="p-2 text-xl">price: {menuitem.price}</p>
+        <p class="text-xl italic">{menuitem.description}</p>
       </div>
     ))}
   </div>
