97126c1155323410cb57c84ec78ec9da83986193
mingosis <tom.piccirillo15@gmail.com>
2024-02-16 12:49:27 +1000

    styling of please login to order

--- client/src/pages/order.jsx
+++ client/src/pages/order.jsx
@@ -135,9 +135,13 @@ const Order = ()  => {
         </main>
       </div>
             ) : (
-              <>
-              <h1>please login to view the order page</h1>
-              </>
+              <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                  Please Login to Order
+                </h2>
+              </div>
+              </div>
             )}
     </div> 
 
